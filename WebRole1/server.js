4801adb5c519243bf86a9cea7b4dd09b348c8bad
Jaime Bueza <jbueza@gmail.com>
2011-12-11 05:27:18 -0800

    Finishing up url shortener service.

--- WebRole1/server.js
+++ WebRole1/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
-//var url = require("./lib/url.js");
-
-var azure     = require("azure")
-  , uuid      = require("node-uuid")
-  , client    = azure.createTableService()
+var azure = require("azure")
+  , alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("")
+  , base = alphabet.length
+  , uuid = require("node-uuid")
+  , client = azure.createTableService()
   , tableName = "abcUrls";
 
 
 var app = module.exports = express.createServer();
 
-app.configure(function(){
+app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.bodyParser());
@@ -20,39 +20,92 @@ app.configure(function(){
 
 // Routes
 
-app.get('/', function(request, response) {
-	response.render('index', { title: 'Vancouver Azure Meetup Group!' });
+app.get('/', function (request, response) {
+  response.render('index', { title: 'Vancouver Azure Meetup Group!' });
 });
-app.post('/api', function(request, response) {
+app.post('/api', function (request, response) {
+  response.header("Content-Type", "application/json");
   url.insert(request, response);
 });
-app.get('/:hash', function(request, response) {
-  
+app.get('/:hash', function (request, response) {
+  if (request.params.hash != "favicon.ico") {
+
+    url.redirect(request.params.hash, response);
+  }
 });
 
 app.listen(process.env.port || 1337);
 
-
-var url = function() {};
+var url = function () { };
 url.insert = function insert(request, response) {
-   client.createTableIfNotExists(tableName, function(err) {
-    var item = request.body.item || {};
-    console.error(request.body);
-    item.RowKey = uuid();
-    item.PartitionKey = '1';
-    item.DateCreated = new Date().getTime();
-    item.url = request.body.url;
+  client.createTableIfNotExists(tableName, function (err) {
 
-    client.insertEntity(tableName, item, function (error) {
-      if(error) console.error(JSON.stringify(error)); 
-      response.end(JSON.stringify({Success: true}));
-    });    
-   });
+    //check if it exists, if it does exist, redirect
+    url.search({ key: "url", value: request.body.url }, function (row) {
+      if (row.url == "/") {
+        var item = request.body.item || {};
+        item.RowKey = uuid();
+        item.PartitionKey = '1';
+        item.url = request.body.url;
+        item.hash = url.encode(new Date().getTime());
+
+        client.insertEntity(tableName, item, function (error, result) {
+          if (error) console.error(JSON.stringify(error));
+          response.end(JSON.stringify({ Success: true, url: result.hash }));
+        });
+      } else {
+        row.url = row.hash;
+        response.end(JSON.stringify(row));
+      }
+    });
+
+  });
 
 };
 
-url.search = function search(request, response) {
-  
+url.search = function search(paramsObj, callback) {
+  client.createTableIfNotExists(tableName, function (creationError) {
+    if (creationError === null) {
+      var query = azure.TableQuery
+                        .select()
+                        .from(tableName)
+                        .where(paramsObj.key + ' eq ?', paramsObj.value);
+      client.queryEntities(query, function (err, rows) {
+        if (rows.length > 0) {
+          callback.call(this, rows[0]);
+        } else {
+          callback.call(this, { url: "/" });
+        }
+      });
+    } else {
+      console.error(error.creationError);
+    }
+  });
+};
+url.redirect = function redirect(hash, response) {
+  url.search({ key: "hash", value: hash }, function (row) {
+    //add analytics here
+    response.redirect(row.url);
+  });
 };
 
+url.encode = function encode(id) {
+  if (id == 0) return alphabet[0];
+  var code = "";
+  while (id > 0) {
+    code += alphabet[id % base];
+    id = parseInt(id / base, 10);
+  }
+  return code.split("").reverse().join("");
+};
+
+url.decode = function decode(code) {
+  var i = 0;
+
+  for (var index = 0, len = code.length; index < len; index++) {
+    var character = code[index];
+    i = i * base + alphabet.indexOf(character);
+  }
+  return i;
+};
 
