b22bf15721f17f57bbd0ad355b37f028e8206c59
Jaime Bueza <jbueza@gmail.com>
2011-12-11 01:09:41 -0800

    Fix ignores and add uuid module.

--- WebRole1/lib/url.js
+++ WebRole1/lib/url.js
@@ -1,12 +1,32 @@
-var azure = require("azure");
+var azure   = require("azure")
+  , uuid    = require("node-uuid")
+  , client  = azure.createTableService();
 
 var url = function(options) {
     
 };
 
 url.insert = function insert(record) {
-    
+  var self = this;
+  var createItem = function (resp, tasklist) {
+      if (!tasklist) {
+          tasklist = [];
+      }
 
+      var count = tasklist.length;
+
+      var item = req.body.item;
+      item.RowKey = uuid();
+      item.PartitionKey = 'partition1';
+      item.completed = false;
+
+      self.client.insertEntity('tasks', item, function (error) {
+          if(error){  
+              throw error;
+          }
+          self.showItems(req, res);
+      });
+  };
 };
 
 url.search = function search(params) {
