bc1ccd821ecf94316bddff288ca11143775a3791
Jaime Bueza <jbueza@gmail.com>
2012-05-18 01:25:03 -0700

    Update example to use Twitter Bootstrap.

---
+++ WebRole1/public/js/app.js
@@ -0,0 +1,12 @@
+﻿var context = $("form");
+
+context.submit(function (event) {
+  $.post("/api", { url: context.find("input[type=url]").val() }, function (data) {
+   var actualUrl = window.location.href + data.url;
+    context.find("span").html("<a target='_blank' href='" + actualUrl + "'>" + actualUrl + "</a>");
+    $("div.output").fadeIn();
+  });
+});
+
+//When a user lands on the page, set the focus to the url input
+$("input[type=url]")[0].focus();
